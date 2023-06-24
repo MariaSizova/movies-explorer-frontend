72bec9ef8e193601cfc0fa06e6de2f45896aa4b7
Maria Sizova <sizova.maria.olegovna@yandex.ru>
2023-06-24 22:57:58 +0300

    Сделана страница 404

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,13 +6,13 @@ function Promo() {
       <div className='promo__content'>
         <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
         <ul className='promo__list'>
-			<li className='promo__list-item'>О проекте</li>
-			<li className='promo__list-item'>Технологии</li>
-			<li className='promo__list-item'>Студент</li>
-		  </ul>
-      </div>
-    </section>
-  )
+          <li className='promo__list-item'>О проекте</li>
+          <li className='promo__list-item'>Технологии</li>
+          <li className='promo__list-item'>Студент</li>
+          </ul>
+          </div>
+          </section>
+          )
 }
 
 export default Promo;
