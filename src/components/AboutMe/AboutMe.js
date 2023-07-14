18f4c4383f1b9f405487895db80d9d4a17566669
Maria Sizova <sizova.maria.olegovna@yandex.ru>
2023-07-14 12:05:33 +0300

    Поправлен корс запросы

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,7 +10,7 @@ const AboutMe = () => {
                 <div className="about-me__text-block">
                     <p className="about-me__text-block-title">Мария</p>
                     <p className="about-me__text-block-subtitle">Фронтенд-разработчик, 26 лет</p>
-                    <p className="about-me__text-block-text"> живу в Москве, закончила бакалавриат в Высшей школе экономике, магистратуру в МГЮА им. Кутафина по направлению юриспруденция. Работаю в Банке Тинькофф налоговым консультантом, с 2023 года решила начать учиться во фронтенд-разработке.</p>
+                    <p className="about-me__text-block-text"> Я живу в Москве, закончила бакалавриат в Высшей школе экономике, магистратуру в МГЮА им. Кутафина по направлению юриспруденция. Работаю в Банке Тинькофф налоговым консультантом, с 2023 года решила начать учиться во фронтенд-разработке.</p>
                     <a className="about-me__text-block-link" href='https://github.com/MariaSizova' target='_blank' rel="noreferrer">Github</a>
                 </div>
                 <img className='about-me__image' src={myPhoto} alt='myPhoto' />
